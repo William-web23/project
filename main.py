6b49b46eac079db0db46bc98faa8f79645937de8
William-web23 <william.henry.dev@gmail.com>
2025-05-22 13:34:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ async def register(data: RegisterRequest):
 # --- Deployment block ---
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
